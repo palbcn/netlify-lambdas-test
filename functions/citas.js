4c6ebbdf1a60dff902c37f530b6cea96e26cca11
PA <palbcn@yahoo.com>
2022-04-29 18:40:06 +0200

    read citasjson inside function handler

--- functions/citas.js
+++ functions/citas.js
@@ -1,14 +1,19 @@
 import fs from "fs/promises";
 
 const dayms = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
-let citas = ["N/A"];
-(async function () {
-  citas = JSON.parse(await fs.readFile("citas.json"));
-})();
 
+export async function handler(event, context) {
+  try {
+      let citas = ["N/A"];
+      try {
+        citas = JSON.parse(await fs.readFile("citas.json"));
+      } catch (err) {
+        return {
+          statusCode: 404,
+          body: "OOps " + JSON.stringify(err)
+        }
+      }
 
-	export async function handler(event, context) {
-    try {  
 
 		let daynum = Math.floor(Date.now() / dayms);
 		let todayidx = daynum % citas.length;
