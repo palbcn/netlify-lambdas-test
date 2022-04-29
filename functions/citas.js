cda7b10eda599caaf4bfa99afaa5ef67486b4336
PA <palbcn@yahoo.com>
2022-04-29 18:53:06 +0200

    require json instead of readfile

--- functions/citas.js
+++ functions/citas.js
@@ -1,31 +1,14 @@
-import fs from "fs/promises";
-
-const dayms = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
-
+import citas from "../citas.json";
+const dayms = 1000 * 60 * 60 * 24;   // 24 hours in milliseconds
 export async function handler(event, context) {
-  try {
-      let citas = ["N/A"];
-      try {
-        citas = JSON.parse(await fs.readFile("citas.json"));
-      } catch (err) {
-        return {
-          statusCode: 404,
-          body: "OOps " + JSON.stringify(err)
-        }
-      }
-
-
-		let daynum = Math.floor(Date.now() / dayms);
-		let todayidx = daynum % citas.length;
-		let cita = `at ${todayidx} for ${daynum} is "${citas[todayidx]}"`;
-		return {
-			statusCode: 200,
-			body: cita
-    }
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: "500 - Big Catch - " + JSON.stringify(err)
-      }
-    }
-	}
+  let daynum = Math.floor(Date.now() / dayms);
+  let todayidx = daynum % citas.length;
+  let cita = `<h1>"${citas[todayidx]}"</h1><p>PA for JI at ${todayidx} for ${daynum}.</p>`;
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html; charset=utf-8"
+    },
+    body: cita
+  }
+}
