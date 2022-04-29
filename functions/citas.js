175e751c47d3ac7bf6fc16f56bac4cd4543c22b1
PA <palbcn@yahoo.com>
2022-04-29 18:37:09 +0200

    readfile instead of fetch citas.json

--- functions/citas.js
+++ functions/citas.js
@@ -1,20 +1,26 @@
-const fetch = require("node-fetch");
-let citas = ["N/A"];
-const dayms = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
-console.log("main");
-let citasjson = process.env.CITASJSON || "/citas.json";
+import fs from "fs/promises";
 
-(async function () {   
-    citas = JSON.parse(await fetch(citasjson));  
-    console.log(`read ${citas.length} citas`);    
-  })();
-  
+const dayms = 1000 * 60 * 60 * 24; // 24 hours in milliseconds
+let citas = ["N/A"];
+(async function () {
+  citas = JSON.parse(await fs.readFile("citas.json"));
+})();
+
+
 	export async function handler(event, context) {
+    try {  
+
 		let daynum = Math.floor(Date.now() / dayms);
 		let todayidx = daynum % citas.length;
 		let cita = `at ${todayidx} for ${daynum} is "${citas[todayidx]}"`;
 		return {
 			statusCode: 200,
 			body: cita
+    }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: "500 - Big Catch - " + JSON.stringify(err)
+      }
     }
 	}
